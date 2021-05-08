b59daee67fbf817c3281564c1ce1991f2a869497
Sharan <michinaux.marko@gmail.com>
2021-05-08 12:00:12 -0400

    Create globalThis.js

---
+++ globalThis.js
@@ -0,0 +1,6 @@
+//In the browser:
+console.log(globalThis);
+// Output: Window {0: Window, 1: global, window: Window, self: Window, document: document, name: "", location: Location, …}
+
+//In NodeJS:
+console.log(globalThis); //returns the global object(global)
